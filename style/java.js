107e4b2b719915f695c74b7fde4f5fe51dda09fc
negartourany <negartoorany283@gmail.com>
2023-07-28 19:57:04 +0200

    almost done! till the 10th video

--- style/java.js
+++ style/java.js
@@ -1,13 +1,47 @@
 "use strict";
+let numb = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector("#guess").textContent;
 document.querySelector("#score").textContent;
 document.querySelector("#highscore").textContent;
 document.querySelector(".question-mark").textContent;
 document.querySelector(".form-number").value;
+document.querySelector("#again-btn").addEventListener("click", function () {
+  numb = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".question-mark").textContent = "?";
+
+  document.querySelector("#score").textContent = 20;
+  document.querySelector(".form-number").value = null;
+  document.querySelector("#guess").textContent = "Start guessing...";
+  document.querySelector("#body").style.backgroundColor = "#d9c5b2";
+  document.querySelector("#square").style.width = "100px";
+});
+
 document.querySelector("#check-btn").addEventListener("click", function () {
   let numberValue = Number(document.querySelector(".form-number").value);
-  if (numberValue < 20 && numberValue !== 0 && numberValue !== null) {
-    console.log(numberValue);
+  if (numberValue <= 20 && numberValue !== 0 && numberValue !== null) {
+    if (numberValue === numb) {
+      document.querySelector("#guess").textContent = "Your guess is correct!🏆";
+      document.querySelector("#body").style.backgroundColor = "#adff2f";
+      document.querySelector("#square").style.width = "13rem";
+      document.querySelector(".question-mark").textContent = numb;
+    } else if (numberValue > numb) {
+      if (score > 0) {
+        document.querySelector("#guess").textContent = "It's too high...";
+        score = score - 1;
+        document.querySelector("#score").textContent = score;
+      } else {
+        document.querySelector("#guess").textContent = "You're a loser!";
+      }
+    } else if (numberValue < numb) {
+      if (score > 0) {
+        document.querySelector("#guess").textContent = "It's too low...";
+        score = score - 1;
+        document.querySelector("#score").textContent = score;
+      } else {
+        document.querySelector("#guess").textContent = "You're a loser!";
+      }
+    }
   } else {
     document.querySelector("#guess").textContent =
       "Please insert a valid number!";
