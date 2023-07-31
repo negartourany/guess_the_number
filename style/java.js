9e917de3c1dd5cf230fed783ec9fa949771d0b36
negartourany <negartoorany283@gmail.com>
2023-07-31 17:40:00 +0200

    implementing high score anddddd it's over!!!

--- style/java.js
+++ style/java.js
@@ -1,22 +1,22 @@
 "use strict";
 let numb = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
 document.querySelector("#guess").textContent;
 document.querySelector("#score").textContent;
 document.querySelector("#highscore").textContent;
 document.querySelector(".question-mark").textContent;
 document.querySelector(".form-number").value;
 document.querySelector("#again-btn").addEventListener("click", function () {
+  score = 20;
+  document.querySelector("#score").textContent = 20;
   numb = Math.trunc(Math.random() * 20) + 1;
   document.querySelector(".question-mark").textContent = "?";
-
-  document.querySelector("#score").textContent = 20;
   document.querySelector(".form-number").value = null;
   document.querySelector("#guess").textContent = "Start guessing...";
   document.querySelector("#body").style.backgroundColor = "#d9c5b2";
   document.querySelector("#square").style.width = "100px";
 });
-
+let score = 20;
+let highscore = 0;
 document.querySelector("#check-btn").addEventListener("click", function () {
   let numberValue = Number(document.querySelector(".form-number").value);
   if (numberValue <= 20 && numberValue !== 0 && numberValue !== null) {
@@ -25,6 +25,10 @@ document.querySelector("#check-btn").addEventListener("click", function () {
       document.querySelector("#body").style.backgroundColor = "#adff2f";
       document.querySelector("#square").style.width = "13rem";
       document.querySelector(".question-mark").textContent = numb;
+      if (highscore < score) {
+        highscore = score;
+        document.querySelector("#highscore").textContent = highscore;
+      }
     } else if (numberValue > numb) {
       if (score > 0) {
         document.querySelector("#guess").textContent = "It's too high...";
